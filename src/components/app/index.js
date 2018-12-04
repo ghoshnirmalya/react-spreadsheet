5cd0e8aa1d0fd58af959144cace52b79b3206ab2
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-12-04 19:16:54 +0530

    Initial commit from Create React App

---
+++ src/components/app/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import styles from "./styles.module.css";
+
+class App extends Component {
+  render() {
+    return <div className={styles.app}>Hello World!</div>;
+  }
+}
+
+export default App;
