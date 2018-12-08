3746fe04d8c9a9a34bfe87ab8ef4ec64500a277b
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-12-08 14:29:18 +0530

    Updates

---
+++ precache-manifest.fa001b974cac9dbde905d47859cd2c5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e107ef6c40ecb11d1f89",
+    "url": "/react-spreadsheet/static/css/main.8a77d5f1.chunk.css"
+  },
+  {
+    "revision": "e107ef6c40ecb11d1f89",
+    "url": "/react-spreadsheet/static/js/main.e107ef6c.chunk.js"
+  },
+  {
+    "revision": "5f2cfe2ca2af018f12a9",
+    "url": "/react-spreadsheet/static/js/1.5f2cfe2c.chunk.js"
+  },
+  {
+    "revision": "5b13605dd3850be379b2",
+    "url": "/react-spreadsheet/static/js/runtime~main.5b13605d.js"
+  },
+  {
+    "revision": "8e4486313657e2a2d5b89cd281f471bc",
+    "url": "/react-spreadsheet/index.html"
+  }
+];
